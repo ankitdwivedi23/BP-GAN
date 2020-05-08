0d4ccc6d3acc2464e72cd1331418e4bb0983eddb
Ankit Dwivedi <adwivedi@microsoft.com>
2020-05-08 07:41:29 -0700

    fix DCGAN architecture

--- ankit/args.py
+++ ankit/args.py
@@ -11,8 +11,10 @@ def get_setup_args():
     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
     parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
     parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-    parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+    parser.add_argument("--sample_interval", type=int, default=400, help="number of batches between image sampling")
+    parser.add_argument("--print_every", type=int, default=50, help="number of iterations between printing training stats")
     parser.add_argument("--input_path", type=str, default="data", help="path to root directory of images")
     parser.add_argument("--output_path", type=str, default="results", help="path to directory for storing model output")
+    parser.add_argument("--num_workers", type=int, default=4, help="number of workers for dataloader")
     
     return parser.parse_args()
