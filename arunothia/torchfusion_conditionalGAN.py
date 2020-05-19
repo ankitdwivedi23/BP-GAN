b4b008e61eb9982667474d178c5f10eee93f3e7f
Arunothia Marappan <armarapp@microsoft.com>
2020-05-19 03:47:50 -0700

    Trying to use torchfusion for conditional GANs

---
+++ arunothia/torchfusion_conditionalGAN.py
@@ -0,0 +1,38 @@
+from torchfusion.gan.learners import *
+from torchfusion.gan.applications import StandardGenerator,StandardProjectionDiscriminator
+from torch.optim import Adam
+from torchfusion.datasets import fashionmnist_loader
+import torch.cuda as cuda
+import torch
+import torch.nn as nn
+from torchvision import datasets 
+import torchvision.transforms as transforms
+
+G = StandardGenerator(output_size=(3,64,64),latent_size=128,num_classes=6)
+D = StandardProjectionDiscriminator(input_size=(3,64,64),apply_sigmoid=False,num_classes=6)
+
+if cuda.is_available():
+    G = nn.DataParallel(G.cuda())
+    D = nn.DataParallel(D.cuda())
+
+g_optim = Adam(G.parameters(),lr=0.0002,betas=(0.5,0.999))
+d_optim = Adam(D.parameters(),lr=0.0002,betas=(0.5,0.999))
+
+
+book_data  = datasets.ImageFolder(root='data/Task2_Split/Task2_Split/train',
+                               transform=transforms.Compose([
+                                   transforms.Resize((64, 64)),
+                                   transforms.ToTensor()
+                           ]))
+
+book_dataset =  torch.utils.data.DataLoader(book_data,
+                                        batch_size=128,
+                                        shuffle=True)
+
+
+dataset = fashionmnist_loader(size=32,batch_size=64)
+
+learner = RStandardGanLearner(G,D)
+
+if __name__ == "__main__":
+   learner.train(book_dataset,num_classes=6,gen_optimizer=g_optim,disc_optimizer=d_optim,save_outputs_interval=500,model_dir="./fashion-gan",latent_size=128,num_epochs=50,batch_log=False)
