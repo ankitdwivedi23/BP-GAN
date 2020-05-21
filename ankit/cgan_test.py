da080b97a6e6c1649eee9a502cebf5f41108f656
Ankit Dwivedi <adwivedi@microsoft.com>
2020-05-21 14:06:02 -0700

    copy Generator class in test file

--- ankit/cgan_test.py
+++ ankit/cgan_test.py
@@ -15,7 +15,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-import cgan
 import util
 import args
 from eval import fid_score
@@ -27,7 +26,38 @@ def main():
     LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
     # Arguments
-    opt = args.get_setup_args()    
+    opt = args.get_setup_args()
+
+    img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+    class Generator(nn.Module):
+        def __init__(self):
+            super(Generator, self).__init__()
+
+            self.label_emb = nn.Embedding(opt.num_classes, opt.num_classes)
+
+            def block(in_feat, out_feat, normalize=True):
+                layers = [nn.Linear(in_feat, out_feat)]
+                if normalize:
+                    layers.append(nn.BatchNorm1d(out_feat, 0.8))
+                layers.append(nn.LeakyReLU(0.2, inplace=True))
+                return layers
+
+            self.model = nn.Sequential(
+                *block(opt.latent_dim + opt.num_classes, 128, normalize=False),
+                *block(128, 256),
+                *block(256, 512),
+                *block(512, 1024),
+                nn.Linear(1024, int(np.prod(img_shape))),
+                nn.Tanh()
+            )
+
+        def forward(self, noise, labels):
+            # Concatenate label embedding and image to produce input
+            gen_input = torch.cat((self.label_emb(labels), noise), -1)
+            img = self.model(gen_input)
+            img = img.view(img.size(0), *img_shape)
+            return img
 
     def eval_fid(fake_images):
         output_images_path = os.path.join(opt.output_path, opt.version, "test")
@@ -48,7 +78,7 @@ def main():
                                     transforms.ToTensor()
                             ]))
 
-    netG = cgan.Generator()
+    netG = Generator()
     netG.cuda()
     netG.load_state_dict(torch.load(os.path.join(model_path, "model.pt")))
     netG.eval()
