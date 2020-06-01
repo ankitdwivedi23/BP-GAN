39fff59212e506dd0b21126307ca76ea354bc8fc
Ankit Dwivedi <adwivedi@microsoft.com>
2020-06-01 13:14:55 -0700

    fix generator

--- ankit/models/acgan.py
+++ ankit/models/acgan.py
@@ -7,31 +7,27 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         
         self.convt1_1 = nn.Sequential(
-            nn.ConvTranspose2d(latent_dim, 384, 4, 1, 0),
-            nn.BatchNorm2d(384),
+            nn.ConvTranspose2d(latent_dim, 192, 4, 1, 0),
+            nn.BatchNorm2d(192),
             nn.LeakyReLU(0.2, inplace=True)
         )
 
         self.convt1_2 = nn.Sequential(
-            nn.ConvTranspose2d(num_classes, 384, 4, 1, 0),
-            nn.BatchNorm2d(384),
+            nn.ConvTranspose2d(num_classes, 192, 4, 1, 0),
+            nn.BatchNorm2d(192),
             nn.LeakyReLU(0.2, inplace=True)
         )
 
         self.conv_layers = nn.Sequential(
-            # ConvT-BN-ReLU-1 (Input => 1 * 1 * 768, Output => 4 * 4 * 384)
-            nn.ConvTranspose2d(in_channels=768, out_channels=384, kernel_size=4, stride=1),
-            nn.BatchNorm2d(384),
-            nn.LeakyReLU(0.2, inplace=True),
-            # ConvT-BN-ReLU-2 (Input => 4 * 4 * 384, Output => 8 * 8 * 192)
+            # ConvT-BN-ReLU-1 (Input => 4 * 4 * 384, Output => 8 * 8 * 192)
             nn.ConvTranspose2d(in_channels=384, out_channels=192, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(192),
             nn.LeakyReLU(0.2, inplace=True),
-            # ConvT-BN-ReLU-3 (Input => 8 * 8 * 192, Output => 16 * 16 * 96)
+            # ConvT-BN-ReLU-2 (Input => 8 * 8 * 192, Output => 16 * 16 * 96)
             nn.ConvTranspose2d(in_channels=192, out_channels=96, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(96),
             nn.LeakyReLU(0.2, inplace=True),
-            # ConvT-BN-ReLU-4 (Input => 16 * 16 * 96, Output => 32 * 32 * 48)
+            # ConvT-BN-ReLU-3 (Input => 16 * 16 * 96, Output => 32 * 32 * 48)
             nn.ConvTranspose2d(in_channels=96, out_channels=48, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(48),
             nn.LeakyReLU(0.2, inplace=True),
