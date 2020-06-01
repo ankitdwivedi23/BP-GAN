e878dc0f08663ed3f2671c546c46816f43d28bad
Ankit Dwivedi <adwivedi@microsoft.com>
2020-06-01 13:05:15 -0700

    print statements for debugging

--- ankit/acgan_train.py
+++ ankit/acgan_train.py
@@ -236,6 +236,9 @@ def main():
             mask = torch.rand((batch_size,), device=device) <= label_noise_prob
             mask = mask.type(torch.float)            
             noisy_label = torch.mul(1-mask, real_label_smooth) + torch.mul(mask, fake_label)
+            
+            print("real pred shape:")
+            print(real_pred.shape)
 
             d_real_loss = (adversarial_loss(real_pred, noisy_label) + auxiliary_loss(real_aux, class_labels)) / 2
 
@@ -252,6 +255,9 @@ def main():
             mask = torch.rand((batch_size,), device=device) <= label_noise_prob
             mask = mask.type(torch.float)            
             noisy_label = torch.mul(1-mask, fake_label) + torch.mul(mask, real_label_smooth)
+
+            print("fake pred shape:")
+            print(fake_pred.shape)
 
             d_fake_loss = (adversarial_loss(fake_pred, noisy_label) + auxiliary_loss(fake_aux, gen_class_labels)) / 2
 
